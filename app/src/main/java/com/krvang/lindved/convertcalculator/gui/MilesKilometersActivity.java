821d1dea152ece95c7fc0d712680051bcfb13859
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2018-03-20 17:43:16 -0700

    Able to switch between converting from Miles to Kilometers and vice versa

--- app/src/main/java/com/krvang/lindved/convertcalculator/gui/MilesKilometersActivity.java
+++ app/src/main/java/com/krvang/lindved/convertcalculator/gui/MilesKilometersActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,7 +20,7 @@ public class MilesKilometersActivity extends AppCompatActivity {
     private TextView mTitleText, mAmountText, mResultText, mPostfixTest;
     private EditText mValueText;
 
-    private boolean mIsCovertingFromMilesToKilometers;
+    private boolean mIsConvertingFromMilesToKilometers;
 
     public static Intent getIntent(Context packageContext){
         Intent intent = new Intent(packageContext, MilesKilometersActivity.class);
@@ -32,6 +33,7 @@ public class MilesKilometersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_miles_kilometers);
 
         initializeTextViews();
+        setMilesToKilometers();
     }
 
     @Override
@@ -45,12 +47,64 @@ public class MilesKilometersActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.menu_switch:{
-
+                switchConvertFrom();
                 return true;
             }
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    /**
+     * Changes if we are converting from Miles to Kilometers or vice versa.
+     */
+    private void switchConvertFrom(){
+        if(mIsConvertingFromMilesToKilometers)
+            setKilometersToMiles();
+        else
+            setMilesToKilometers();
+        mIsConvertingFromMilesToKilometers = !mIsConvertingFromMilesToKilometers;
+    }
+
+    /**
+     * Sets what should be displayed if we are converting from Miles to Kilometers.
+     */
+    private void setMilesToKilometers(){
+        mTitleText.setText(R.string.milesToKilometersTitle);
+        try{
+            float value = Float.parseFloat(mValueText.getText().toString());
+            String messagedToBeDisplayed = value + " " + getString(R.string.amountMiles);
+            mAmountText.setText(messagedToBeDisplayed);
+            mPostfixTest.setText(R.string.kilometers);
+        }catch (NumberFormatException nfe){
+            Log.e(TAG, "setMilesToKilometers: Value is not a float - Can be ignored");
+            setNoValueEntered();
+        }
+    }
+
+    /**
+     * Sets what should be displayed if we are converting from Kilometers to Miles.
+     */
+    private void setKilometersToMiles(){
+        mTitleText.setText(R.string.kilometersToMiles);
+        try{
+            float value = Float.parseFloat(mValueText.getText().toString());
+            String messagedToBeDisplayed = value + " " + getString(R.string.amountKilometers);
+            mAmountText.setText(messagedToBeDisplayed);
+            mPostfixTest.setText(R.string.miles);
+        }catch (NumberFormatException nfe){
+            Log.e(TAG, "setKilometersToMiles: Value is not a float - Can be ignored");
+            setNoValueEntered();
+        }
+    }
+
+    /**
+     * Sets what should be displayed if no value has been entered by the user.
+     */
+    private void setNoValueEntered(){
+        mAmountText.setText(R.string.noValue);
+        mResultText.setText("");
+        mPostfixTest.setText(R.string.pressCalculate);
     }
 
     private void initializeTextViews(){
